2b491d5324f6c0bd443f26ffb8cc558094d9bf36
冨永尚樹 <tominaganaoki@tominaganaokinoMacBook-Air.local>
2026-03-28 16:50:16 +0900

    fix: remove unused variables

--- src/components/GlobalNavigation.tsx
+++ src/components/GlobalNavigation.tsx
@@ -8,7 +8,6 @@ export const GlobalNavigation: React.FC = () => {
     // トップ画面の場合は表示しない
     const isTop = location.pathname === '/'
 
-
     // 現在の画面が「患者トップ（ステータス画面）」かどうか。手技選択などの子画面なら患者トップに戻るボタンを出す。
     const isInsidePatient = location.pathname.startsWith('/treatment-scan/')
     // ※/treatment-scan/:id のURLからIDを抽出して戻るため
